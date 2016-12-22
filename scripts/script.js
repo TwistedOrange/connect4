9c808babb2e3e2af5571ccf7a932375b57add7c8
Deb-S <debfrombc@gmail.com>
2016-12-21 21:04:44 -0800

    player name input + token chosen works

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,48 @@ console.log('hi js');
 
 
 function selectToken(token) {
-  token == 'green' ? console.log('selected green token') : console.log('selected purple token');
+  token == 'token1' ? console.log('selected green token') : console.log('selected purple token');
+
+}
+
+/*
+Manages the game data: score, player name, current player, player's token
+*/
+const manageData = {
+  currentPlayer: 1,
+  tokens: [ 'green', 'purple'],
+  currentToken: 0,       // tokens[0],
+  gameWon: false,
+  score: [ 0, 0 ],
+  playerNames: ['player1', 'player2'],
+
+  resetGame: function () {
+    // clears all game data for new game
+    this.currentPlayer = 1;
+    this.currentToken = token[0];
+    gameWon = false;
+    score = Array(2).fill(0);
+    $('#board').html();
+  }
+};
+
+const managePlayer = {
+  savePlayerName: function(pname) {
+    console.log('savePlayerName() for player: ' + pname);
+  }
+};
+
+const manageBoard = {
+
+};
+
+const manageUI = {
+
+};
+
+/*
+// Initiate events to listen for various actions when on each page load
+*/
+window.onload = function() {
+  // $('#colMarker').
 }
